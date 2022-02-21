885178f81e6f240e2990ef96e6065e7cd04413a0
Leandran <leandranm@outlook.com>
2022-02-21 22:55:40 +0200

    Add resource and seed data

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
+  before_action :authenticate_user!, only: [:app] #user must be logged in to view app
   def home
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+user = User.first || User.create(email: 'test@test.com', password: 'password', password_confirmation: 'password')
+
+posts = [
+  {
+    title: 'My first post', 
+    content: 'Windows or Mac'
+  },
+  {
+    title: 'My second post', 
+    content: 'Android or Ios'
+  },
+  {
+    title: 'My third post',
+    content: 'Linux or Windows'
+  }
+]
+
+
+posts.each do |post_data|
+  user.posts.create(post_data)
+end
